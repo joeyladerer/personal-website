34fca76ac68b1409c7a35b7e0eff77f7036d9ec1
Joey Laderer <joeyladerer@me.com>
2021-02-20 17:05:26 -0800

    re-did layout options

--- src/Organisms/HomePage.js
+++ src/Organisms/HomePage.js
@@ -4,12 +4,14 @@ import HomePhoto from "../Molecules/HomePhoto";
 import '../CSS Files/HomePage.css'
 import HomeNameAndDescription from "../Molecules/HomeNameAndDescription";
 import HomeNavBar from "../Molecules/HomeNavBar";
-import ProfileImage from '../Images/mainprofilephoto.png'
+import ProfileImage from '../Images/mainprofilebest.png'
 
 function HomePage() {
     return (
         <Box>
+            <Center>
             <HomeNavBar/>
+            </Center>
             <Center>
             <div className={"Profile-Image"}>
                 <img src={ProfileImage}/>
